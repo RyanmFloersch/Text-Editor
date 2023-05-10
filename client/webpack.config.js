37a0b1e2fbce77ae548aec1fc7e427cc5f156b91
Ryan <ryanmfloersch@gmail.com>
2023-05-09 20:20:47 -0400

    Worked on adding the rules and plugins to the webpack config. Also updated the scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -18,13 +19,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'Text Editor',
+        inject: 'body',
+        template: './index.html'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Text-Editor',
+        short_name: 'texEdit',
+        description: 'My text editor application',
+        background_color: '#808080',
+        theme_color: '#FFFFFF',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin()
     ],
-
     module: {
       rules: [
-        
+        //loader rule. test uses a regex for css file. Checkign the end of each file for the css tag. 
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.(js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env']
+              ]
+            }
+          }
+        }
       ],
     },
-  };
-};
+    devServer: {
+      proxy: {
+        '*': 'http://localhost:3333'
+      },
+      compress: true,
+      hot: true,
+      watchFiles: ['./index.html']
+    },
+
+  }
+
+}
